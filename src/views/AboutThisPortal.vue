cf567c39a1b052389d245a6fd940da06d579bf19
Aditya Harshvardhan <aditya.harshvardhan@jacks.sdstate.edu>
2021-05-23 18:13:54 -0500

    Changed About This Portal page layout + Animations

--- src/views/AboutThisPortal.vue
+++ src/views/AboutThisPortal.vue
@@ -9,46 +9,54 @@
                     <p> This portal was made possible by the following: </p>
                 </div>
                 <div class = "row">
-                    <ul class="list-unstyled">
-                        <li class="media">
-                            <img class="mr-3 image-2" src="../assets/images/vuejs.png" alt="Generic placeholder image">
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">Vue.js</h5>
-                                <p> A JavaScript framework (like Angular, React) for UI developent (launched in 2015). </p>
-                            </div>
-                        </li>
-                        <li class="media my-4">
-                            <img class="mr-3 image-2" src="../assets/images/html.png" alt="Generic placeholder image">
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">HTML</h5>
-                                <p> Used in displaying things on this portal. </p>
-                            </div>
-                        </li>
-                        <li class="media">
-                            <img class="mr-3 image-2" src="../assets/images/css.png" alt="Generic placeholder image">
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">CSS</h5>
-                                <p> Everything pretty, including colors, themes and page transitions on this portal. </p>
-                            </div>
-                        </li>
-                        <li class="media">
-                            <img class="mr-3 image-2" src="../assets/images/js.png" alt="Generic placeholder image">
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">JavaScript</h5>
-                                <p> All the different pages connected through a router JavaScript file (utilizing VueRouter).</p>
-                            </div>
-                        </li>
-                        <li class="media">
-                            <img class="mr-3 image-2" src="../assets/images/bootstrap.png" alt="Generic placeholder image">
-                            <div class="media-body">
-                                <h5 class="mt-0 mb-1">Bootstrap</h5>
-                                <p> A CSS framework for responsive and mobile-friendly web development. </p>
-                            </div>
-                        </li>
-                    </ul>
+                    <div class="card animation1" style="width: 18rem;">
+                        <img class="card-img-top" src = '../assets/images/vuejs.png' alt="Card image cap"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Vue.js</h5>
+                            <p class="card-text">
+                               A JavaScript framework (like Angular, React) for UI developent (launched in 2015).
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card animation1" style="width: 18rem;">
+                        <img class="card-img-top" src = '../assets/images/html.png' alt="Card image cap"/>
+                        <div class="card-body">
+                            <h5 class="card-title">HTML</h5>
+                            <p class="card-text">
+                               Used in displaying things on this portal.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card animation1" style="width: 18rem;">
+                        <img class="card-img-top" src = '../assets/images/css.png' alt="Card image cap"/>
+                        <div class="card-body">
+                            <h5 class="card-title">CSS</h5>
+                            <p class="card-text">
+                               Everything pretty, including colors, themes and page transitions on this portal.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card animation1" style="width: 18rem;">
+                        <img class="card-img-top" src = '../assets/images/js.png' alt="Card image cap"/>
+                        <div class="card-body">
+                            <h5 class="card-title">JavaScript</h5>
+                            <p class="card-text">
+                               All the different pages connected through a router JavaScript file (utilizing VueRouter).
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card animation1" style="width: 18rem;">
+                        <img class="card-img-top" src = '../assets/images/bootstrap.png' alt="Card image cap"/>
+                        <div class="card-body">
+                            <h5 class="card-title">Bootstrap</h5>
+                            <p class="card-text">
+                               A CSS framework for responsive and mobile-friendly web development.
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div class = "row">
-                    <p>Note - All the above names are Trademarks of their respective owners</p>
+                    <p class = "padding">Note - All the above names are Trademarks of their respective owners</p>
                 </div>
             </div>
         </div>
@@ -57,4 +65,17 @@
 
 <style scoped>
     @import '../assets/css/global.css';
+    .card{
+        margin-top:2%;
+        margin-left: 6%;
+        padding: 2%;
+        background-color: rgb(116, 116, 116);
+    }
+    .animation1{
+        animation-name: slideLeft;
+        animation-duration: 1s;
+    }
+    .padding{
+        padding: 2%;
+    }
 </style>
